b39f1b4b68ebf305dd05bc19dab8006ea86ca85b
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-11-22 02:03:11 -0500

    feat: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviePageComponent } from './pages/movie-page/movie-page.component';
 
 const routes: Routes = [
   {path:'', component:LoginPageComponent},
   {path:'signup', component:SignupPageComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: 'movie/:id', component: MoviePageComponent},
 
 ];
 
