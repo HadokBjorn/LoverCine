dd767b1ce852e444e028cb85d2d81a576d27b9c6
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-11-22 02:09:43 -0500

    feat: sign up page

--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Toast } from 'src/app/utils/toast.util';
 
 @Component({
   selector: 'app-signup-page',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup-page.component.sass']
 })
 export class SignupPageComponent {
-
+  constructor(private toastService: ToastrService){}
+  toast = new Toast(this.toastService)
+  signupForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    image: new FormControl('', [Validators.required, Validators.pattern(/(https?:\/\/.*\.(?:png|jpg|jpeg|gif))/i)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  })
+  get username(){
+    return this.signupForm.get('username')
+  }
+  get email(){
+    return this.signupForm.get('email')
+  }
+  get image(){
+    return this.signupForm.get('image')
+  }
+  get password(){
+    return this.signupForm.get('password')
+  }
+  signUp(){
+    console.log(this.signupForm.value);
+    if(!this.username?.value){this.toast.alertError('username is required')}
+    if(!this.email?.value){ this.toast.alertError('email is required')}
+    if(!this.image?.value){this.toast.alertError('image is required')}
+    if(!this.password?.value){this.toast.alertError('username is required')}
+    
+    if(this.signupForm.invalid) return
+    
+  }
 }
