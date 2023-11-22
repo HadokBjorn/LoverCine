4978f5f4d1267c949968962394b3364f827df321
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-11-22 02:04:35 -0500

    chore: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +10,9 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from "@angular/material/icon"
+import { MatIconModule } from "@angular/material/icon";
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviePageComponent } from './pages/movie-page/movie-page.component';
 
 
 @NgModule({
@@ -16,12 +21,18 @@ import { MatIconModule } from "@angular/material/icon"
     LoginPageComponent,
     SignupPageComponent,
     LogoComponent,
+    HomePageComponent,
+    MoviePageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
